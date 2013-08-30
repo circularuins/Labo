075536173b3deeb56fea88e99e7e45b8259f47e7
circularuins <ubuntu11@circularuins.(none)>
2013-08-30 13:47:10 +0900

    とりあえずコード貼って保存できてブラウザで実行できるとこまで実装

--- lib/Labo/PC/C/Entry.pm
+++ lib/Labo/PC/C/Entry.pm
@@ -29,8 +29,44 @@ sub create {
         }
     }
 
-print Dumper $id;
     my $url = '/entry/show?id=' . $id->{row_data}->{id};
+    return $c->redirect($url);
+}
+
+sub update {
+    my ( $class, $c ) = @_;
+    my $id = $c->request->param('id');
+    if ( my $body = $c->request->param('body') ) {
+#        $body =~ s%<br />%&lt;br /&gt;%g;
+#        $body =~ s%\r\n%<br />%g;
+        my $now = time;
+        if ( my $title = $c->request->param('title') ) {
+            $c->db->update(
+                'entry' => {
+                    title => $title,
+                    body  => $body,
+                    utime => $now,
+                },
+                {
+                    id => $id,
+                }
+            );
+        }
+        else {
+            $c->db->update(
+                'entry' => {
+                    title => '無題',
+                    body  => $body,
+                    utime => $now,
+                },
+                {
+                    id => $id,
+                }
+            );
+        }
+    }
+
+    my $url = '/entry/show?id=' . $id;
     return $c->redirect($url);
 }
 
@@ -44,4 +80,14 @@ sub show {
         entries => \@entries,
     });
 }
+
+sub js_eval {
+    my ($class, $c) = @_;
+    my $query = $c->{request}->{env}->{QUERY_STRING};
+    my ($id) = $query =~ /id=(\d+)/;
+    my @entries = $c->db->search('entry', {'id' => $id});
+
+    $c->render('js_eval.tt', { entries => \@entries });
+}
+
 1;
